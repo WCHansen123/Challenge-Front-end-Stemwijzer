2e249b0f3d8a817b1530189ef8b3aa84c616c585
Wilco <wilco.hansen@outlook.com>
2020-02-11 14:40:43 +0100

    testing css framework with test data

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Stemwijzer</title>
+</head>
+<body>
+    <div class="container white">
+        <div class="row">
+            <div class="col s8">
+                <img src="assets/img/stemwijzerpng.png" class="fixed" alt="">
+            </div>
+            <div class="col s4"><?php include 'templates/parties.php'?></div>
+        </div>
+    </div>
+</body>
+</html>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="assets/js/main.js"></script>
+
+
+
+<!--<div class="col-12">-->
+<!--    <div class="row">-->
+<!--        <img src="assets/img/stemwijzerpng.png" class="fixed" alt="">-->
+<!--    </div>-->
+<!--    <div class="col-5">-->
+<!---->
+<!--    </div>-->
+<!--</div>-->
+<!---->
+<!--<p class="flow-text">Challenge-Front-end-Stemwijzer 2020</p>-->
